072fae8b38be01015e582ca809719786866faefa
LUTFI DWI PUTRA <lutfidp13@gmail.com>
2026-01-12 08:14:31 +0700

    Belajar Tipe Data

---
+++ Main.py
@@ -0,0 +1,136 @@
+# greeting = "Wongkon"
+# print(greeting)
+
+# print("Helloo world")
+
+# addition  = 2*10
+# result = addition - 1
+# print(result)
+
+# name = input("Masukkan Nama Anda : ")
+
+# name = input("Masukkan Nama Anda : ")
+# print(name)
+
+# var = 11
+# print(var)
+# print(id(var))
+# var = 12
+# print(var)
+# print(id(var))
+
+# X = True
+# print(type(X))
+
+# X = False
+# print(type(X))
+
+# X = "Dicoding"
+# print(type(X))
+
+# multi_line = """Halo!
+# Saya adalah Lutfi
+# Saya 17 tahun"""
+
+# print(multi_line)
+
+# X = "Aku Adalah Dicoding"
+# print(X[0])
+
+# x = "Labubu Ta Iki?"
+# print(x[3:])
+
+# name = "Lutfi Dwi Putra"
+# print(f"Hallo Nama Panjang ku Adalah {name}")
+
+# x = [ 1, 1.1, True, "aku"]
+# print(type(x))
+
+# x = [ 1, 1.1, True, False, "Dicoding"]
+# print(x[4])
+
+# x = [9, 2.2, "Dicoding"]
+# x[0] = "Indonesia"
+
+# print(x)
+
+# x = "Laptop", "HP", "Radio", "TWS", "Labubu", "Helikopter", "Mobil"
+
+# print(x[0])
+# print(x[2])
+# print(x[4])
+
+# x = "Laptop", "Labubu", "Kamis", "HP", "Babi", "Monyet", "Kambing"
+
+# print(x[0:5:2])
+# print(x[1:])
+# print(x[:3])
+
+# x = (1, "Aku Manusia", 1+5j)
+# print(type(x))
+
+# x = (9, "Labubu", 1+3j)
+# print(x[0:3])
+
+# x = {1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 3, 5}
+# print(x)
+# print(type(x))
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+
+# union = set1.union(set2)
+# print("Union :", union)
+
+# inteersection = set1.intersection(set2)
+# print("intersection :", inteersection)
+
+# x = { "Name" : "Labubu Hitam", "Age" : 17, "IsMarried" : False}
+# print(type(x))
+
+
+# x = { "Name" : "Labubu Hitam", "Age" : 17, "IsMarried" : False}
+# print(x["Name"])
+
+
+# x = { "Name" : "Labubu Hitam", "Age" : 17, "IsMarried" : False}
+# x ["Job"] = "Homeless"
+
+# print(x)
+
+
+# x = { "Name" : "Labubu Hitam", "Age" : 17, "IsMarried" : False}
+# del x["IsMarried"]
+
+# print(x)
+
+
+# x = { "Name" : "Labubu Hitam", "Age" : 17, "IsMarried" : False}
+# x ["Name"] = "Dicoding"
+
+# print(x)   
+
+# print(float(5))
+# print(int(5.9))
+# print(int(4.5))
+# print(str(45))
+# print(float(30))
+# print(str(5.5))                                                        
+# print(set([1, 5, 6, 8, 9]))
+# print(tuple([10,11,12]))
+# print(list("1,2,3,4"))
+
+# print(dict([[1,2],[3,4]]))
+
+# data_diri = { "firstName" : "Lutfi", "lastName" : "Dwi Putra", "age" : 18, "isMarried" : False }
+# print(data_diri)
+
+# kata = "dicoding"
+# kata = kata.upper()
+# print(kata)
+
+# kata = "LABUBU"
+# kata = kata.lower()
+# print(kata)
+
+print("DiCoDiNg         ".rstrip())
